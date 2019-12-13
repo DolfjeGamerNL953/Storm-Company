6e684e751142ad21694ee01bd33469f1d41928a0
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-13 16:28:18 +0100

    Summary

--- index.js
+++ index.js
@@ -54,28 +54,6 @@ fs.readdir("./Admin C/", (err, files) => {
 
 })
 
-fs.readdir("./commands/", (err, files) => {
-
-    if (err) console.log(err);
-
-    var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-    if (jsFiles.length <= 0) {
-        console.log("Kon geen files vinden");
-        return;
-    }
-
-    jsFiles.forEach((f, i) => {
-
-        var fileGet = require(`./commands/${f}`);
-        console.log(`De file ${f} is geladen`);
-
-        bot.commands.set(fileGet.help.name, fileGet);
-
-    })
-
-})
-
 fs.readdir("./Bot C/", (err, files) => {
 
     if (err) console.log(err);
