76c1c7d4dc658ced3622aa59f270f9f1c78db7e3
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-17 18:36:09 +0100

    Summary

--- Gebruiker C/help.js
+++ Gebruiker C/help.js
@@ -5,22 +5,8 @@ module.exports.run = async (bot, message, args) => {
     var testembed = new discord.RichEmbed()
     .setDescription("Help Commands")
     .setColor("#ff000")
-    .addField("!help > Laat deze lijst met commands zien\n\n !ip > Laat info zien over de Alpha Server")
-    .addField("!ticket > Maak een ticket a an")
-    .addField("!goedemiddag, morgen, avond > Krijg een leuk berichtje van de bot")
-    .addField("!eten, slapen > Krijg een leuk berichtje van de bot")
-    .addField("!idee > Plaats een idee")
-    .addField("!level > Laat huidige level zien")
-    .addField("!report > Maak een report")
-    .addField("!userinfo > Laat jou info zien")
-    .addField("!botinfo > Laat info over de bot zien")
-    .addField("!review > Plaats een review")
-    .addField("!crew > Laat het company team zien")
-    .addField("!ping > Laat je ping zien")
-    .addField("!sps > Speel steen, papier, schaar met de bot")
-    .addField("**© 2019 StormCompany**");
-
-    message.channel.send(testembed);
+    .addField("!help > Laat deze lijst met commands zien\n\ !ip > Laat info zien over de Alpha Server \n !ticket > Maak een ticket a aan \n !idee > Plaats een idee \n !level > Laat huidige level zien \n !report > Maak een report \n !userinfo > Laat jou info zien \n !botinfo > Laat info over de bot zien \n !review > Plaats een review \n !crew > Laat het company team zien \n !ping > Laat je ping zien \n !sps > Speel steen, papier, schaar met de bot \n !goedemiddag, morgen, avond > Krijg een leuk berichtje van de bot \n !eten, slapen > Krijg een leuk berichtje van de bot")
+    .setFooter("**© 2019 StormCompany**");
 }
 
 module.exports.help = {
