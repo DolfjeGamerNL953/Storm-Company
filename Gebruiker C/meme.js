f6af0cdf765b716befe3764773bfc33c08b8d33f
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-20 10:15:45 +0100

    Summary

---
+++ Gebruiker C/meme.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports.run = async (bot, message, args) => {
+    const subReddits = ["dankmeme", "meme", "me_irl"];
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+    const img = await randomPuppy(random);
+    var memeembed = new discord.RichEmbed()
+    .setColor("RANDOM")
+    .setImage(img)
+    .setTitle(`From /r/${random}`)
+    .setURL(`https://reddit.com/r/${random}`);
+
+    message.channel.send(memeembed);
+}
+
+module.exports.help = {
+    name: "meme"
+}
