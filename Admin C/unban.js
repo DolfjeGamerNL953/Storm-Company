3c90b654f3f3569c1df82264c599d81034c839d5
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-20 11:09:23 +0100

    Update unban.js

--- Admin C/unban.js
+++ Admin C/unban.js
@@ -1,15 +1,16 @@
-exports.run = (client, message, args) => {
-    let reason = args.slice(1).join(' ');
-    client.unbanReason = reason;
+module.exports.run = (client, message, args) => {
+    let reden = args.slice(1).join(' ');
+    client.unbanreden = reden;
     client.unbanAuth = message.author;
     let user = args[0];
     let unbanlog = client.channels.find('name', 'unban-logs');
     if (!unbanlog) return message.reply('I cannot find a unban-logs channel');
-    if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
+    if (reden.length < 1) return message.reply('You must supply a reden for the unban.');
     if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
     message.guild.unban(user);
   };
-  
+
+
   exports.conf = {
     enabled: true,
     guildOnly: false,
@@ -17,8 +18,8 @@ exports.run = (client, message, args) => {
     permLevel: 0
   };
   
-  exports.help = {
+  module.exports.help = {
     name: 'unban',
     description: 'Unbans the user.',
-    usage: 'unban [mention] [reason]'
+    usage: 'unban [gebruiker] [reden]'
   };
