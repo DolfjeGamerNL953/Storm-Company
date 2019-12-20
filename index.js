075564bc0f0a026e892e03ec8149d16382bb0af0
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-20 13:35:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,28 @@ fs.readdir("./Gebruiker C/", (err, files) => {
 
 })
 
+fs.readdir("./Help C/", (err, files) => {
+
+    if (err) console.log(err);
+
+    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+    if (jsFiles.length <= 0) {
+        console.log("Kon geen files vinden");
+        return;
+    }
+
+    jsFiles.forEach((f, i) => {
+
+        var fileGet = require(`./Help C/${f}`);
+        console.log(`De file ${f} is geladen`);
+
+        bot.commands.set(fileGet.help.name, fileGet);
+
+    })
+
+})
+
 fs.readdir("./Admin C/", (err, files) => {
 
     if (err) console.log(err);
