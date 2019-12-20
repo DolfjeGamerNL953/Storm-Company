e9632db856243e52c1a903b2203d05b066bd7c1c
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-20 14:22:34 +0100

    Summary

--- Help C/helpadmin.js
+++ Help C/helpadmin.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
     var testembed = new discord.RichEmbed()
     .setDescription("Help Commands")
-    .setColor("#ff000")
-    .addField("!helpadmin > Laat deze lijst met commands zien\n\ !ban > Verban een gebruiker door \n !ticket > Maak een ticket a aan \n !idee > Plaats een idee \n !level > Laat huidige level zien \n !report > Maak een report \n !userinfo > Laat jou info zien \n !botinfo > Laat info over de bot zien \n !review > Plaats een review \n !crew > Laat het company team zien \n !ping > Laat je ping zien \n !sps > Speel steen, papier, schaar met de bot \n !goedemiddag, morgen, avond > Krijg een leuk berichtje van de bot \n !eten, slapen > Krijg een leuk berichtje van de bot")
+    .setColor("RANDOM")
+    .addField("!helpadmin > Laat deze lijst met commands zien\n\ !ban > Verban een gebruiker \n !clear -> Verwijderd een bepaald aantal berichten \n !giveaway -> Maak een giveaway aan (LEAD ONLY) \n !kick -> Kick een gebruiker \n !mededeling -> Maak een mededeling aan (LEAD ONLY) \n !prefix -> Verander de bot prefix (LEAD ONLY) \n !tempban -> Verban een gebruiker voor een bepaalde tijd \n !tempmute -> Mute een gebruiker voor een bepaalde tijd \n !ticketclose > Sluit een ticket \n !unban -> Verwijder een verbanning (LEAD ONLY) \n !unmute -> Verwijder een mute (LEAD ONLY) \n !warn -> Geef een gebruiker een waarschuwing")
     .setFooter("**© 2019 StormCompany**");
 }
 
