02709eb2afec02e365e900c47b1928d7121a3d1f
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-20 13:31:32 +0100

    Summary

--- Help C/help.js
+++ Help C/help.js
@@ -2,14 +2,21 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var helpembed = new discord.RichEmbed()
-        .setDescription("Help Commands")
-        .setColor("#ff000")
-        .addField("!help > Laat deze lijst met commands zien\n\ !ip > Laat info zien over de Alpha Server \n !ticket > Maak een ticket a aan \n !idee > Plaats een idee \n !level > Laat huidige level zien \n !report > Maak een report \n !userinfo > Laat jou info zien \n !botinfo > Laat info over de bot zien \n !review > Plaats een review \n !crew > Laat het company team zien \n !ping > Laat je ping zien \n !sps > Speel steen, papier, schaar met de bot \n !goedemiddag, morgen, avond > Krijg een leuk berichtje van de bot \n !eten, slapen > Krijg een leuk berichtje van de bot")
+    var icon = message.guild.iconURL;
+
+    var serverEmbed = new discord.RichEmbed()
+        .setDescription("GamesNL Community Server Info")
+        .setColor("#00f7ff")
+        .setThumbnail(icon)
+        .addField("Servernaam:", message.guild.name)
+        .addField("U bent op deze server gekomen op:", message.member.joinedAt)
+        .addField("Totaal members:", message.guild.memberCount)
         .setFooter("**© 2019 StormCompany**");
 
-    return message.channel.send(helpembed);
+    return message.channel.send(serverEmbed);
+
 }
+
 
 module.exports.help = {
     name: "help"
