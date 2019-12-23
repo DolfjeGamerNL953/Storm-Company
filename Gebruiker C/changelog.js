2f117ff15aabf5d8a019d0dd28d277ae7e89a0ef
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-23 18:59:56 +0100

    Summary

--- Gebruiker C/changelog.js
+++ Gebruiker C/changelog.js
@@ -3,12 +3,14 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     var month = message.guild.date
-    var testembed = new discord.RichEmbed()
-    .setDescription("Help Commands")
+    var changelogembed = new discord.RichEmbed()
+    .setDescription("Changelog")
     .setColor("#ff000")
-    .addField("Maand:", month)
+    .addField("Hudige Maand:", month)
     .addField("Bot is gemaakt")
     .setFooter("**© 2019 StormCompany**");
+
+    message.channel.send(changelogembed)
 }
 
 module.exports.help = {
--- Gebruiker C/updates.js
+++ Gebruiker C/updates.js
@@ -2,11 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var testembed = new discord.RichEmbed()
+    var updatesembed = new discord.RichEmbed()
     .setDescription("Updates StormCompany Bot")
     .setColor("RANDOM")
     .addField("December: \n De bot is gemaakt")
     .setFooter("**© 2019 StormCompany**");
+
+    message.channel.send(updatesembed)
 }
 
 module.exports.help = {
