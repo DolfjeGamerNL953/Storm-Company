3914c8ae408b777bb40675018d6739319b0b6306
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-25 17:54:43 +0100

    Update quiz.js

--- Gebruiker C/quiz.js
+++ Gebruiker C/quiz.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args, msg) => {
             number = 3;
             var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
             switch(random) {
-                case 1: message.channel.send("Hoe oud is de bot: \nA 5j \nB65j \nC421j \n5000j"); cAnswer = "a"; break; 
+                case 1: message.channel.send("Hoe oud is de bot: \nA 5j \nB65j \nC421j \nD5000j"); cAnswer = "a"; break; 
                 case 2: message.channel.send("hoeveel testen zijn er nu"); cAnswer = "5"; break;
                 case 3: message.channel.send("When was Killroy created?"); cAnswer = "1940"; break;
         
