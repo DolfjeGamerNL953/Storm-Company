a526bfcec5d8510acd4d17ea66a63dcfebf6c810
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-25 11:31:48 +0100

    Update prijzen.js

--- Gebruiker C/prijzen.js
+++ Gebruiker C/prijzen.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("Prijs Aantallen: \n Eten: Per 64, 32 of per stuk \n Vita: Per 16 of per stuk")
     .setColor("RANDOM")
     .addField("Prijzen Pakketen:", "Storm Vita Pakket: €10.927,70 \n Storm Pro Pakket: €4.700,00 \n Storm Fruit Pakket: €1.005,20 \n Storm Snoep Pakket: €944,60 \n Storm Overig Pakket: €729,20")
-    .addField("Prijzen Eten:", "€10")
+    .addField("Prijzen Eten:", "Lolly rood: \n Per 64: €249,20 \n Per 32: €124,70 \n Per stuk: €4,00 \n Jellybeans: \n Per 64: €249,20 \n Per 32: €124,70 \n Per stuk: €4,00 \n\n Zuurstok: \n Per 64: €451,20 \n Per 32: €225,70 \n Per stuk: €7,10")
     .setFooter("© 2019 StormCompany");
 
     message.channel.send(prijzenEmbed)
