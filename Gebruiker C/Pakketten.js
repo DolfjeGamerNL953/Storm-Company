cf47a15d4de4070b6fed41cffdd8ebb031891171
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-25 16:58:00 +0100

    Update Pakketen.js

--- Gebruiker C/Pakketten.js
+++ Gebruiker C/Pakketten.js
@@ -9,11 +9,11 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("De pakket inhouden")
         .setColor("RANDOM")
         .setThumbnail(icon)
-        .addField("Storm Vita Pakket:", "64x Vita A \n\n 64x Vita B \n\n 64x Vita C \n\n 64x Vita D")
-        .addField("Storm Pro Pakket:", "64x Zalm \n\n 64x Pizza \n\n 64x \n\n  64x Hamburger \n\n 64x Bacon \n\n 64x Sushi \n\n 64x Golden Appels")
-        .addField("Storm Fruit Pakket:", "64x Appel \n\n 64x Ananas \n\n 64x Water")
-        .addField("Storm Snoep Pakket", "64x Lolly rood \n\n 64x Jellybenas \n\n 64x Zuurstok")
-        .addField("Storm Overig Pakket", "64x Flesjes \n\n 64x Water flesjes \n\n 64x Boekjes")    
+        .addField("Storm Vita Pakket:", "64x Vita A \n 64x Vita B \n 64x Vita C \n\ 64x Vita D")
+        .addField("Storm Pro Pakket:", "64x Zalm \n 64x Pizza \n 64x \n  64x Hamburger \n 64x Bacon \n 64x Sushi \n 64x Golden Appels")
+        .addField("Storm Fruit Pakket:", "64x Appel \n 64x Ananas \n 64x Water")
+        .addField("Storm Snoep Pakket", "64x Lolly rood \n 64x Jellybenas \n 64x Zuurstok")
+        .addField("Storm Overig Pakket", "64x Flesjes \n 64x Water flesjes \n 64x Boekjes")    
         .setFooter("© 2019 StormCompany");
 
 
