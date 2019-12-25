dc408db01ac451ea26c55fb19ac6f812d7f321ea
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-25 11:27:57 +0100

    Update prijzen.js

--- Gebruiker C/prijzen.js
+++ Gebruiker C/prijzen.js
@@ -3,9 +3,10 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     var prijzenEmbed = new discord.RichEmbed()
-    .setDescription("Prijzen")
+    .setTitle("Prijzen")
+    .setDescription("Prijs Aantallen: \n Eten: Per 64, 32 of per stuk \n Vita: Per 16 of per stuk")
     .setColor("RANDOM")
-    .addField("Prijzen Pakketen:", "Storm Snoep Pakket: €944,60 \n Storm Fruit Pakket: €1.000,10")
+    .addField("Prijzen Pakketen:", "Storm Vita Pakket: €10.927,70 \n Storm Pro Pakket: €4.700,00 \n Storm Fruit Pakket: €1.005,20 \n Storm Snoep Pakket: €944,60 \n Storm Overig Pakket: €729,20")
     .addField("Prijzen Eten:", "€10")
     .setFooter("© 2019 StormCompany");
 
