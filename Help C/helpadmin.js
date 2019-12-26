046c878a7d431d0887b6749644d40e5036924f63
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-26 09:06:09 +0100

    Update helpadmin.js

--- Help C/helpadmin.js
+++ Help C/helpadmin.js
@@ -2,11 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    var text = ("!tempmute -> Mute een gebruiker voor een bepaalde tijd \n !ticketclose > Sluit een ticket \n !unban -> Verwijder een verbanning (LEAD ONLY) \n !unmute -> Verwijder een mute (LEAD ONLY) \n !warn -> Geef een gebruiker een waarschuwing")
+
     var helpadminEmbed = new discord.RichEmbed()
         .setTitle("Help Commands")
         .setDescription("!helpadmin voor de lijst met admin commands")
         .setColor("RANDOM")
-        .addField("!tempmute -> Mute een gebruiker voor een bepaalde tijd")
+        .addField(text)
         .setFooter("**© 2019 StormCompany**");
 
     message.channel.send(helpadminEmbed)
