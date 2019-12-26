c2c76c83fbcaa9f2f4ee1f06828b5a2362bf744e
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-26 09:40:45 +0100

    Summary

--- Gebruiker C/ideediscord.js
+++ Gebruiker C/ideediscord.js
@@ -15,8 +15,13 @@ module.exports.run = async (bot, message, args) => {
         .addField("Discordidee: ", idee)
         .addField("Ingezonden door: ", message.author);
  
-        message.author.send("U heeft succesvol een idee geschreven, u idee:", ideeEmbed)
-  
+        var userideeEmbed = new discord.RichEmbed()
+        .setTitle("Idee")
+        .setDescription(":white_check_mark: = Goedgekeurd \n :x: = Afgekeurd \n :question: = er word over nagedacht \n :soon: = Het word binnenkort uitgevoerd")
+        .setColor("RANDOM")
+        .addField("Idee:", idee);
+        message.author.send("U heeft succesvol een idee geschreven, u idee:", userideeEmbed)
+        
         // Vind het kanaal.
     var ideeChannel = message.guild.channels.find(`name`, "ideeën-discord");
     if (!ideeChannel) return message.guild.send("Kan het kanaal niet vinden");
