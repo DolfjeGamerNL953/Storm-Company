93c9ff3260139ddf0a602e948e1497069aca1112
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-26 09:03:03 +0100

    Update helpadmin.js

--- Help C/helpadmin.js
+++ Help C/helpadmin.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
         .setTitle("Help Commands")
         .setDescription("!helpadmin voor de lijst met admin commands")
         .setColor("RANDOM")
-        .addField("!ban > Verban een gebruiker \n !clear -> Verwijderd een bepaald aantal berichten \n !giveaway -> Maak een giveaway aan (LEAD ONLY) \n !kick -> Kick een gebruiker \n !mededeling -> Maak een mededeling aan (LEAD ONLY) \n !prefix -> Verander de bot prefix (LEAD ONLY) \n !tempban -> Verban een gebruiker voor een bepaalde tijd \n !tempmute -> Mute een gebruiker voor een bepaalde tijd \n !ticketclose > Sluit een ticket \n !unban -> Verwijder een verbanning (LEAD ONLY) \n !unmute -> Verwijder een mute (LEAD ONLY) \n !warn -> Geef een gebruiker een waarschuwing")
+        .addField("!tempmute -> Mute een gebruiker voor een bepaalde tijd")
         .setFooter("**© 2019 StormCompany**");
 
     message.channel.send(helpadminEmbed)
