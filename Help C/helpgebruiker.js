dbf96de93da6223461ba3a072959d0ca86d0a4b0
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-26 09:08:59 +0100

    Summary

--- Help C/helpgebruiker.js
+++ Help C/helpgebruiker.js
@@ -2,11 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    var gebruikertext = ("!ideecomp => Plaats een company idee \n !ideediscord => Plaats een discord idee \n !ideeweb => Plaats een website idee \n !info => Laat informatie zien \n !ip => Laat info zien over de Alpha Server \n !level => Laat je huidige level zien \n !pakketten => Laat de pakketten met inhoud zien \n !prijzen => Laat de prijzen zien \n !report => Rapporteer een gebruiker \n !review => Plaats een review \n !ticket => Maak een ticket aan \n !userinfo => Laat info over user zien")
+
     var helpgebruikerEmbed = new discord.RichEmbed()
     .setTitle("Help Commands")
     .setDescription("!helpgebruiker voor de lijst commands")
     .setColor("RANDOM")
-    .addField("!helpgebruiker > Laat deze lijst met commands zien \n\ !ideecomp => Plaats een company idee \n !ideediscord => Plaats een discord idee \n !ideeweb => Plaats een website idee \n !info => Laat informatie zien \n !ip => Laat info zien over de Alpha Server \n !level => Laat je huidige level zien \n !pakketten => Laat de pakketten met inhoud zien \n !prijzen => Laat de prijzen zien \n !report => Rapporteer een gebruiker \n !review => Plaats een review \n !ticket => Maak een ticket aan \n !userinfo => Laat info over user zien")
+    .addField(gebruikertext)
     .setFooter("**© 2019 StormCompany**");
 
     message.channel.send(helpgebruikerEmbed)
