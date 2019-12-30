0ba2dd5262cd795f2283f99038d4fcb89420e699
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2019-12-30 12:26:09 +0100

    Update roles.js

--- Server C/roles.js
+++ Server C/roles.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot, message, args) => {
 
     await message.delete().catch(O_o => { });
 
-    const a = message.guild.roles.get('661167241026600991') //verification
-    const b = message.guild.roles.get('661167257296306196') //ban
-    const c = message.guild.roles.get('661167274505404421') //game
+    const a = message.guild.roles.get('659706141521215488') //verification
+    const b = message.guild.roles.get('659840807297351753') //ban
+    const c = message.guild.roles.get('659841093541560320') //game
 
     const filter = (reaction, user) => ['A', 'B', 'C'].includes(reaction.emoji.name) && user.id === message.author.id;
 
