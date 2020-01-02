54949ecba52e5116815d90730cef26b28fafe8ed
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-02 12:05:21 +0100

    Update Pakketten.js

--- Gebruiker C/Pakketten.js
+++ Gebruiker C/Pakketten.js
@@ -13,7 +13,8 @@ module.exports.run = async (bot, message, args) => {
         .addField("Storm Pro Pakket:", "64x Zalm \n 64x Pizza \n 64x \n  64x Hamburger \n 64x Bacon \n 64x Sushi \n 64x Golden Appels")
         .addField("Storm Fruit Pakket:", "64x Appel \n 64x Ananas \n 64x Water")
         .addField("Storm Snoep Pakket", "64x Lolly rood \n 64x Jellybenas \n 64x Zuurstok")
-        .addField("Storm Overig Pakket", "64x Flesjes \n 64x Water flesjes \n 64x Boekjes")    
+        .addField("Storm Overig Pakket", "64x Flesjes \n 64x Water flesjes \n 64x Boekjes") 
+        .addField("Storm Kerst Kleding:", "8x Kersttrui rendier \n 8x Lange kersttrui rendier \n 8x Kersttrui kerstboom \n 8x Kersttrui kerstman \n 8x lange kersttrui kerstman")   
         .setFooter("© 2019 StormCompany");
 
 
@@ -21,5 +22,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "pakketen"
+    name: "pakketten"
 }
