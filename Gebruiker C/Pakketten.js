183e4e88bf957350627458ca42d85005add372b0
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-02 12:40:50 +0100

    Summary

--- Gebruiker C/Pakketten.js
+++ Gebruiker C/Pakketten.js
@@ -15,6 +15,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Storm Snoep Pakket", "64x Lolly rood \n 64x Jellybenas \n 64x Zuurstok")
         .addField("Storm Overig Pakket", "64x Flesjes \n 64x Water flesjes \n 64x Boekjes") 
         .addField("Storm Kerst Kleding:", "8x Kersttrui rendier \n 8x Lange kersttrui rendier \n 8x Kersttrui kerstboom \n 8x Kersttrui kerstman \n 8x lange kersttrui kerstman")   
+        .addField("Storm Head Pakket:", "16x Blaze Head \n 16x Panda head \n 16x Romeo30 Head \n 16x Jordystorm Head")
         .setFooter("© 2019 StormCompany");
 
 
