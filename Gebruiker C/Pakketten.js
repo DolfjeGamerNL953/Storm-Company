175278649d55f3baf2b31a2b66d70c79fdf30fac
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-02 12:56:03 +0100

    Update pakketten.js

--- Gebruiker C/Pakketten.js
+++ Gebruiker C/Pakketten.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor("RANDOM")
         .setThumbnail(icon)
         .addField("Storm Vita Pakket:", "64x Vita A \n 64x Vita B \n 64x Vita C \n\ 64x Vita D")
-        .addField("Storm Pro Pakket:", "64x Zalm \n 64x Pizza \n 64x \n  64x Hamburger \n 64x Bacon \n 64x Sushi \n 64x Golden Appels")
+        .addField("Storm Pro Pakket:", "64x Zalm \n 64x Pizza \n 64x Friet \n 64x Hamburger \n 64x Bacon \n 64x Sushi \n 64x Golden Appels")
         .addField("Storm Fruit Pakket:", "64x Appel \n 64x Ananas \n 64x Water")
         .addField("Storm Snoep Pakket", "64x Lolly rood \n 64x Jellybenas \n 64x Zuurstok")
         .addField("Storm Overig Pakket", "64x Flesjes \n 64x Water flesjes \n 64x Boekjes") 
