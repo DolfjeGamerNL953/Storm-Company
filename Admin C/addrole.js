2f75b16639a5b323cec6ca29fe5f7bafc3495d93
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-19 09:54:16 +0100

    Update addrole.js

--- Admin C/addrole.js
+++ Admin C/addrole.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     if(!rMember) return message.reply("Gebruiker niet gevonden")
     var role = args.join(" ").slice(22);
     if(!role) return message.reply("Deze role bestaat niet");
-    var gRole = message.guild.roles.find(`name`, verificatie);
+    var gRole = message.guild.roles.find(`name`, verification);
     if(!gRole) return message.reply("De role is niet gevonden");
 
     if(rMember.roles.has(gRole.id));
