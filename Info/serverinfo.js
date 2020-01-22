21ba28a9c702240b2be56df97f32852c80c4028c
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-22 17:09:23 +0100

    Update serverinfo.js

--- Info/serverinfo.js
+++ Info/serverinfo.js
@@ -4,16 +4,13 @@ module.exports.run = async (bot, message, args) => {
 
     var icon = message.guild.iconURL;
 
-    var serverEmbed = new discord.RichEmbed()
-        .setDescription("GamesNL Community Server Info")
-        .setColor("RANDOM")
-        .setThumbnail(icon)
-        .addField("Servernaam:", message.guild.name)
-        .addField("U bent op deze server gekomen op:", message.member.joinedAt)
-        .addField("Totaal members:", message.guild.memberCount)
-        .setFooter("**© 2019 StormCompany**");
+    var vertific = ['None', 'Low', 'Medium', 'High', 'Exetreme'];
 
-    return message.channel.send(serverEmbed);
+    const embed = new discord.RichEmbed()
+    .setDescription(`${message.guild.name}'s Info`)
+    .setThumbnail(icon)
+
+    message.channel.send(embed)
 
 }
 
