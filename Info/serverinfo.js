32d71a6303faec9105bc00e4230f42ca8e2b2104
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-22 17:20:08 +0100

    Update serverinfo.js

--- Info/serverinfo.js
+++ Info/serverinfo.js
@@ -9,7 +9,19 @@ module.exports.run = async (bot, message, args) => {
     const embed = new discord.RichEmbed()
     .setDescription(`${message.guild.name}'s Info`)
     .setThumbnail(icon)
-
+    .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
+    .setTimestamp()
+    .addField('**Server Name:**', message.guild.name, true)
+    .addField('**Server ID', message.guild.id)
+    .addField('**Server Owner:**', message.guild.owner.user)
+    .addField('**Owner ID:**', message.guild.owner.id)
+    .addField('**Members:**', message.guild.memberCount)
+    .addField('**Channels:', `${message.guild.channels.filter(channel => channel.type === 'voice').size}` voice / `${message.guild.channels.filter(channel => channel.type === 'text').size} text`, true)
+    .addField('**Verification Level:**', `${vertific[message.guild.veriFicationLevel]}`, true)
+    .addField('**Server Region**', message.guild.region, true)
+    .addField('**AFK Channel:**', message.guild.afkChannel, true)
+    .addField('**Created:**', message.guild.createdAt, true)
+    .setColor('RANDOM');
     message.channel.send(embed)
 
 }
