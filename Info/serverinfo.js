3c8c6923cf1a33c3c3dafde346476dbd03d52cd6
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-22 19:41:16 +0100

    update serverinfo.js

--- Info/serverinfo.js
+++ Info/serverinfo.js
@@ -2,27 +2,18 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var icon = message.guild.iconURL;
+    // var icon = message.guild.iconURL;
 
-    var vertific = ['None', 'Low', 'Medium', 'High', 'Exetreme'];
+    // var serverEmbed = new discord.RichEmbed()
+    //     .setDescription("GamesNL Community Server Info")
+    //     .setColor("RANDOM")
+    //     .setThumbnail(icon)
+    //     .addField("Servernaam:", message.guild.name)
+    //     .addField("U bent op deze server gekomen op:", message.member.joinedAt)
+    //     .addField("Totaal members:", message.guild.memberCount)
+    //     .setFooter("**© 2019 StormCompany**");
 
-    const embed = new discord.RichEmbed()
-    .setDescription(`${message.guild.name}'s Info`)
-    .setThumbnail(icon)
-    .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
-    .setTimestamp()
-    .addField('**Server Name:**', message.guild.name, true)
-    .addField('**Server ID', message.guild.id)
-    .addField('**Server Owner:**', message.guild.owner.user)
-    .addField('**Owner ID:**', message.guild.owner.id)
-    .addField('**Members:**', message.guild.memberCount)
-    .addField('**Channels:', `${message.guild.channels.filter(channel => channel.type === 'voice').size}` voice / `${message.guild.channels.filter(channel => channel.type === 'text').size} text`, true)
-    .addField('**Verification Level:**', `${vertific[message.guild.veriFicationLevel]}`, true)
-    .addField('**Server Region**', message.guild.region, true)
-    .addField('**AFK Channel:**', message.guild.afkChannel, true)
-    .addField('**Created:**', message.guild.createdAt, true)
-    .setColor('RANDOM');
-    message.channel.send(embed)
+    // return message.channel.send(serverEmbed);
 
 }
 
