0cfb7303560a9b92ed7bf42cc278d7a471b1674d
DolfjeGamerNL <57659549+DolfjeGamerNL@users.noreply.github.com>
2020-01-26 17:21:48 +0100

    Summary

--- Gebruiker/ticket.js
+++ Gebruiker/ticket.js
@@ -2,91 +2,92 @@ const discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
 
-    var prefix = '!'
+    message.channel.send("**Tijdelijk Uitgeschakeld** maak nu een ticket aan door middel van -ticket en dan ziet u een verder help menu!")
+    // var prefix = '!'
 
-    if (!args[0]) return message.channel.send(`Gebruik: ${prefix}ticket <reden>.`);
+    // if (!args[0]) return message.channel.send(`Gebruik: ${prefix}ticket <reden>.`);
 
-    var reason = args.join(" ").slice(22);
+    // var reason = args.join(" ").slice(22);
  
-    // ID van de categorie van de tickets.
-    const categoryId = "651114524149022730";
+    // // ID van de categorie van de tickets.
+    // const categoryId = "651114524149022730";
  
-    // Verkrijg Gebruikersnaam
-    var userName = message.author.username;
-    // Verkrijg discriminator
-    var userDiscriminator = message.author.discriminator;
+    // // Verkrijg Gebruikersnaam
+    // var userName = message.author.username;
+    // // Verkrijg discriminator
+    // var userDiscriminator = message.author.discriminator;
  
-    // Als ticket al gemaakt is
-    var bool = false;
+    // // Als ticket al gemaakt is
+    // var bool = false;
  
-    // Kijk na als ticket al gemaakt is.
-    message.guild.channels.forEach((channel) => {
+    // // Kijk na als ticket al gemaakt is.
+    // message.guild.channels.forEach((channel) => {
  
-        // Als ticket is gemaakt, zend bericht.
-        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
+    //     // Als ticket is gemaakt, zend bericht.
+    //     if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
  
-            message.channel.send("Je hebt al een ticket aangemaakt");
+    //         message.channel.send("Je hebt al een ticket aangemaakt");
  
-            bool = true;
+    //         bool = true;
 
-        }
+    //     }
  
-    });
+    // });
  
-    // Als ticket return code.
-    if (bool == true) return;
+    // // Als ticket return code.
+    // if (bool == true) return;
  
-    var embedCreateTicket = new discord.RichEmbed()
-    .setDescription("Ticket")
-    .setTitle("Hai," + message.author.username)
-    .setFooter("Support kanaal is aangemaakt")
-    .setColor("RANDOM");
+    // var embedCreateTicket = new discord.RichEmbed()
+    // .setDescription("Ticket")
+    // .setTitle("Hai," + message.author.username)
+    // .setFooter("Support kanaal is aangemaakt")
+    // .setColor("RANDOM");
  
-    message.channel.send(embedCreateTicket);
+    // message.channel.send(embedCreateTicket);
 
     
-    var embedCreateTicketlog = new discord.RichEmbed()
-    .setDescription("Ticket")
-    .setTitle("Hai," + message.author.username)
-    .setFooter("Support kanaal is aangemaakt")
-    .setColor("#00eeff")
-    .addField("**© 2019 StormCompany**");
+    // var embedCreateTicketlog = new discord.RichEmbed()
+    // .setDescription("Ticket")
+    // .setTitle("Hai," + message.author.username)
+    // .setFooter("Support kanaal is aangemaakt")
+    // .setColor("#00eeff")
+    // .addField("**© 2019 StormCompany**");
 
  
-        var Ticketlogchannel = message.guild.channels.find("name", "ticket-logs");
-        if (!Ticketlogchannel) return message.guild.send("Het kanaal is niet gevonden");
-        Ticketlogchannel.send(embedCreateTicketlog);
+    //     var Ticketlogchannel = message.guild.channels.find("name", "ticket-logs");
+    //     if (!Ticketlogchannel) return message.guild.send("Het kanaal is niet gevonden");
+    //     Ticketlogchannel.send(embedCreateTicketlog);
 
-    // Maak kanaal en zet in juiste categorie.
-    message.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
+    // // Maak kanaal en zet in juiste categorie.
+    // message.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
  
-        createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
+    //     createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
  
-            // Zet perms voor iedereen
-            settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
-            // Zet perms voor de gebruiker die ticket heeft aangemaakt.
-            settedParent.overwritePermissions(message.author, {
+    //         // Zet perms voor iedereen
+    //         settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
+    //         // Zet perms voor de gebruiker die ticket heeft aangemaakt.
+    //         settedParent.overwritePermissions(message.author, {
  
-                "READ_MESSAGES": true, "SEND_MESSAGES": true,
-                "ATTACH_FILES": true, "CONNECT": true,
-                "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+    //             "READ_MESSAGES": true, "SEND_MESSAGES": true,
+    //             "ATTACH_FILES": true, "CONNECT": true,
+    //             "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
  
-            });
+    //         });
  
-            var embedParent = new discord.RichEmbed()
-                .setTitle("Hoi, " + message.author.username.toString())
-                .setDescription("Zet hier je vraag/bericht")
-                .setColor('RANDOM')
-                .addField("Reden", reason);
+    //         var embedParent = new discord.RichEmbed()
+    //             .setTitle("Hoi, " + message.author.username.toString())
+    //             .setDescription("Zet hier je vraag/bericht")
+    //             .setColor('RANDOM')
+    //             .addField("Reden", reason);
  
-            settedParent.send(embedParent);
-        }).catch(err => {
-            message.channel.send("Er is iets fout gelopen.");
-        });
+    //         settedParent.send(embedParent);
+    //     }).catch(err => {
+    //         message.channel.send("Er is iets fout gelopen.");
+    //     });
  
-    }).catch(err => {
-        message.channel.send("Er is iets fout gelopen.");
-    });
+    // }).catch(err => {
+    //     message.channel.send("Er is iets fout gelopen.");
+    // });
  
 }
  
